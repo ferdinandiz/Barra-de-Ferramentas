8b3f7af874077148c9248e4776bbc68665b03624
Fernando Gonçalves Abadia <ferdinandiz@gmail.com>
2022-10-21 14:28:11 -0300

    texto editável na barra
    foi elaborado uma barra de texto editável e altura da barra travada.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -57,6 +57,18 @@ namespace Barra_de_Ferramentas
         }
     }
 
+
+    public partial class CustomTextBox : TextBox
+    {
+        public CustomTextBox()
+        {
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            BackColor = Color.Transparent;
+        }
+    }
+
+
+
     partial class Form1
     {
         /// <summary>
@@ -79,71 +91,126 @@ namespace Barra_de_Ferramentas
             base.Dispose(disposing);
         }
 
+        protected override void SetBoundsCore(
+        int x, int y, int width, int height, BoundsSpecified specified)
+        {
+            
+            if ((specified & BoundsSpecified.Height) == 0 || height == 40)
+            {
+                base.SetBoundsCore(x, y, width, 40, specified);
+            }
+            else
+            {
+                return; // RETURN WITHOUT DOING ANY RESIZING
+            }
+        }
+
         #region Windows Form Designer generated code
 
         /// <summary>
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
+        /// 
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Size size = TextRenderer.MeasureText(textBox1.Text, textBox1.Font);
+            textBox1.Width = size.Width;
+            textBox1.Height = size.Height;
+        }
+
         private void InitializeComponent()
         {
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            CustomTextBox tBox = new CustomTextBox();
+
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
+
+            //escondendo a barra de tarefa
+            Taskbarra.Hide();
+            
             // 
             // pictureBox1
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 401);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1920, 39);
+            this.pictureBox1.Size = new System.Drawing.Size(1920, 40);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(678, 401);
+            this.pictureBox2.Location = new System.Drawing.Point(679, 0);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(0);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(937, 39);
+            this.pictureBox2.Size = new System.Drawing.Size(961, 40);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.Parent = this.pictureBox1;
-            this.pictureBox2.Anchor = AnchorStyles.Left | AnchorStyles.Bottom | AnchorStyles.Right;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(555, 401);
+            this.pictureBox3.Location = new System.Drawing.Point(screenWidth-280, 0);
             this.pictureBox3.Margin = new System.Windows.Forms.Padding(0);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(1920, 39);
+            this.pictureBox3.Size = new System.Drawing.Size(1920, 40);
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
-            this.pictureBox3.Parent = this.pictureBox2;
-            this.pictureBox3.Anchor = AnchorStyles.Left | AnchorStyles.Bottom | AnchorStyles.Right;
-            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            //
+            // TextBox1
+            //
+            tBox.Anchor = AnchorStyles.Right | AnchorStyles.Bottom;
+            tBox.Location = new Point(screenWidth-125, 3);
+            tBox.Multiline = true;
+            tBox.Name = "textBox1";
+            tBox.Width = 87;
+            tBox.Size = new System.Drawing.Size(80, 40);
+            tBox.Font = new Font(tBox.Font.FontFamily, tBox.Font.Size * 1.1f);
+            tBox.TabIndex = 4;
+            tBox.BorderStyle = BorderStyle.None;
+            tBox.TabStop = false;
+            tBox.BackColor = System.Drawing.ColorTranslator.FromHtml("#192640");
+            tBox.ForeColor = Color.WhiteSmoke;
+            tBox.TextAlign = HorizontalAlignment.Center;
+            this.Controls.Add(tBox);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(833, 450);
+            this.ClientSize = new System.Drawing.Size(screenWidth, 40);
+            this.MinimumSize = new Size(0, 80);
+            this.MaximumSize = new Size(2*screenWidth, 80);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.Text = "Form1";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            
+            
+
+            //this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
@@ -156,5 +223,6 @@ namespace Barra_de_Ferramentas
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
         private PictureBox pictureBox3;
+        private TextBox textBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace Barra_de_Ferramentas
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
